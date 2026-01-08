0704527eb6392c3dfdf83f5217c4148193b36576
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2026-01-08 16:21:52 -0700

    rename filewatcher package to config and add parser for ReverseProxyConfig

--- config/filewatcher.go
+++ config/filewatcher.go
@@ -1,4 +1,4 @@
-package filewatcher
+package config
 
 import (
 	"bytes"
---
+++ config/parser.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/goccy/go-yaml"
+)
+
+type ReverseProxyConfig struct {
+	Listeners []listener `yaml:"listeners"`
+}
+type listener struct {
+	Listen string  `yaml:"listen"`
+	Routes []route `yaml:"routes"`
+}
+type route struct {
+	Match match `yaml:"match"`
+}
+type match struct {
+	PathPrefix string `yaml:"path_prefix"`
+}
+
+func ParseConfig(file []byte) (*ReverseProxyConfig, error) {
+	cfg := &ReverseProxyConfig{}
+	err := yaml.Unmarshal(file, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
