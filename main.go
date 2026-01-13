0a63d886c23cd21de2459daf5dfda2f885fd185d
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2026-01-12 19:43:45 -0700

    refactor proxy engine to support graceful shutdown and enhance file watcher integration

--- main.go
+++ main.go
@@ -2,43 +2,43 @@ package main
 
 import (
 	"context"
-	"edgegate/filewatcher"
+	egproxy "edgegate/proxy"
 	"flag"
-	"log"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	/*listenAddr := flag.String("listen", ":8080", "listen address")
 	upstreamURL := flag.String("upstream", "http://localhost:8000", "upstream base URL")*/
-	configPath := flag.String("config_path", "./edgegate.yaml", "config path")
+	/*	configPath := flag.String("config_path", "./edgegate.yaml", "config path")
 
-	watcher := filewatcher.NewFileWatcher(*configPath)
-	ctx, cancel := context.WithCancel(context.Background())
-	go watcher.Watch(ctx)
-	defer cancel()
+		watcher := config.NewFileWatcher(*configPath)
+		ctx, cancel := context.WithCancel(context.Background())
+		go watcher.Watch(ctx)
+		defer cancel()
 
-	go func() {
-
-		for {
-			select {
-			case err := <-watcher.ErrorCh:
-				log.Println(err)
-			case filedata := <-watcher.BytesCh:
-				log.Printf("new file data %s\n", filedata)
-			default:
-
-			}
+		file, err := os.ReadFile(*configPath)
+		if err != nil {
+			log.Println(err)
+			return
 		}
-	}()
-	select {}
+		_, err = config.ParseConfig(file)
+		if err != nil {
+			log.Println(err)
+		}*/
+	/*upstreamURL := flag.String("upstream", "https://example.com", "upstream base URL")
+	upstream, _ := url.Parse(*upstreamURL)
 
-	/*upstream, _ := url.Parse(*upstreamURL)
-
-	proxy := egproxy.NewProxy(upstream)
+	proxy := httputil.NewSingleHostReverseProxy(upstream)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		proxy.ServeHTTP(writer, request)
 	})
 	log.Println("listening on :8080 ->", upstream.String())
-	log.Fatal(http.ListenAndServe(*listenAddr, mux))*/
+	log.Fatal(http.ListenAndServe(":8080", mux))*/
+	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	configPath := flag.String("config_path", "./edgegate.yaml", "config path")
+	egproxy.StartEngine(rootCtx, *configPath)
 }
--- proxy/proxyengine.go
+++ proxy/proxyengine.go
@@ -3,15 +3,36 @@ package egproxy
 import (
 	"context"
 	"edgegate/config"
+	"errors"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
+	"sync"
+	"time"
 )
 
+var (
+	servers []*proxyServer
+	mu      sync.Mutex
+)
+
+type proxyServer struct {
+	server *http.Server
+	done   chan struct{}
+}
+
 func StartEngine(ctx context.Context, configPath string) {
-	/*fw := config.NewFileWatcher(configPath)
+	go func() {
+		<-ctx.Done()
+		shutdownActiveServers()
+	}()
+
+	fw := config.NewFileWatcher(configPath)
+	fw.ReturnBytesOnInit = true
+
 	fw.FileChangedHandler = func(file []byte) {
+		shutdownActiveServers()
+
 		cfg, err := config.ParseConfig(file)
 		if err != nil {
 			log.Print(err)
@@ -19,27 +40,20 @@ func StartEngine(ctx context.Context, configPath string) {
 		}
 		initEngine(cfg)
 	}
+
 	fw.ErrorHandler = func(err error) {
 		log.Print(err)
 	}
-	fw.Watch(ctx)*/
-	file, err := os.ReadFile(configPath)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	cfg, err := config.ParseConfig(file)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	initEngine(cfg)
+
+	fw.Watch(ctx)
 }
+
 func initEngine(cfg *config.ReverseProxyConfig) {
+	newServers := make([]*proxyServer, 0)
+
 	for _, l := range cfg.Listeners {
-		serv := &http.Server{
-			Addr: l.Listen,
-		}
+		serv := &http.Server{Addr: l.Listen}
+
 		mux := http.NewServeMux()
 		for _, r := range l.Routes {
 			upstream, err := url.Parse(r.Upstream)
@@ -53,15 +67,55 @@ func initEngine(cfg *config.ReverseProxyConfig) {
 			mux.Handle(proxy.Prefix, proxy)
 		}
 		serv.Handler = mux
-		go startServer(serv)
+
+		ps := &proxyServer{
+			server: serv,
+			done:   make(chan struct{}),
+		}
+		newServers = append(newServers, ps)
+		go startServer(ps)
 	}
-	select {}
+
+	mu.Lock()
+	servers = newServers
+	mu.Unlock()
 }
-func startServer(serv *http.Server) {
-	log.Printf("Starting server on address: %v", serv.Addr)
-	err := serv.ListenAndServe()
-	defer serv.Close()
-	if err != nil {
+
+func startServer(ps *proxyServer) {
+	log.Printf("Starting server on address: %v\n", ps.server.Addr)
+	err := ps.server.ListenAndServe()
+	close(ps.done)
+
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Print(err)
 	}
+
+	log.Printf("server on %v stopped\n", ps.server.Addr)
+}
+
+func shutdownActiveServers() {
+	mu.Lock()
+	old := servers
+	servers = nil
+	mu.Unlock()
+
+	if len(old) == 0 {
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(old))
+
+	for _, s := range old {
+		s := s
+		go func() {
+			defer wg.Done()
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			_ = s.server.Shutdown(shutdownCtx)
+			<-s.done
+		}()
+	}
+
+	wg.Wait()
 }
