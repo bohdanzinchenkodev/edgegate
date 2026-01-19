04f50e1f8b0f32972d34f69b50e9bd3d19d44257
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2026-01-18 20:09:20 -0700

    refactor: shorten config flag name from 'config_path' to 'conf'

--- cmd/edgegate/main.go
+++ cmd/edgegate/main.go
@@ -39,6 +39,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", mux))*/
 	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-	configPath := flag.String("config_path", "./configs/edgegate.yaml", "config path")
+	configPath := flag.String("conf", "./configs/edgegate.yaml", "config path")
+	flag.Parse()
 	egproxy.StartEngine(rootCtx, *configPath)
 }
