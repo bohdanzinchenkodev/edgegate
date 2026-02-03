90e86f52385f5fd675ffb4f5c4d7ace635212e55
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2026-02-03 16:11:49 -0700

    add rate limit policy configuration to listener in parser

--- internal/config/parser.go
+++ internal/config/parser.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/goccy/go-yaml"
 )
 
@@ -8,8 +10,9 @@ type ReverseProxyConfig struct {
 	Listeners []Listener `yaml:"listeners"`
 }
 type Listener struct {
-	Listen string  `yaml:"listen"`
-	Routes []route `yaml:"routes"`
+	Listen    string          `yaml:"listen"`
+	Routes    []route         `yaml:"routes"`
+	RateLimit rateLimitPolicy `yaml:"rate_limit"`
 }
 type route struct {
 	Match    match  `yaml:"match"`
@@ -18,6 +21,19 @@ type route struct {
 type match struct {
 	PathPrefix string `yaml:"path_prefix"`
 	Host       string `yaml:"host"`
+}
+type rateLimitPolicy struct {
+	Enabled bool `yaml:"enabled"`
+
+	// Allow N requests per window (classic "10 req / 1s")
+	Requests int           `yaml:"requests"` // e.g. 10
+	Window   time.Duration `yaml:"window"`   // e.g. 1s, 1m
+
+	// How long to keep an idle client in memory (TTL)
+	ClientTTL time.Duration `yaml:"client_ttl"` // e.g. 5m
+
+	// Trust forwarded headers only from these CIDRs
+	TrustedProxies []string `yaml:"trusted_proxies"`
 }
 
 func ParseConfig(file []byte) (*ReverseProxyConfig, error) {
