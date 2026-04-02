8e0dad24de4ca5929f4fda1d0a570868a8f04264
Bohdan Zinchenko <bohdanzinchenkodev@gmail.com>
2026-04-01 19:49:50 -0600

    refactor GatewayReconciler to remove gatewayName; update Reconcile method to use req.NamespacedName

--- internal/gateway/controller.go
+++ internal/gateway/controller.go
@@ -15,35 +15,35 @@ import (
 
 const controllerName = "edgegate.io/gateway-controller"
 
-// ApplyConfigFunc is the function the controller calls to apply new config.
-// In production, this will be egproxy.ApplyConfig.
 type ApplyConfigFunc func(cfg *config.ReverseProxyConfig)
 
 type GatewayReconciler struct {
 	client      client.Client
-	gatewayName client.ObjectKey
 	applyConfig ApplyConfigFunc
 }
 
-func NewGatewayReconciler(c client.Client, gatewayName client.ObjectKey, applyFn ApplyConfigFunc) *GatewayReconciler {
+func NewGatewayReconciler(c client.Client, applyFn ApplyConfigFunc) *GatewayReconciler {
 	return &GatewayReconciler{
 		client:      c,
-		gatewayName: gatewayName,
 		applyConfig: applyFn,
 	}
 }
 
 func (gr *GatewayReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	var gw gwv1.Gateway
-	if err := gr.client.Get(ctx, gr.gatewayName, &gw); err != nil {
+	if err := gr.client.Get(ctx, req.NamespacedName, &gw); err != nil {
 		return reconcile.Result{}, client.IgnoreNotFound(err)
 	}
 
-	if gw.Spec.GatewayClassName != controllerName {
+	// Fetch the GatewayClass to verify this Gateway belongs to us.
+	var gc gwv1.GatewayClass
+	if err := gr.client.Get(ctx, client.ObjectKey{Name: string(gw.Spec.GatewayClassName)}, &gc); err != nil {
+		return reconcile.Result{}, err
+	}
+	if string(gc.Spec.ControllerName) != controllerName {
 		return reconcile.Result{}, nil
 	}
 
-	// List all HTTPRoutes and filter for ones referencing this Gateway.
 	var httpRouteList gwv1.HTTPRouteList
 	if err := gr.client.List(ctx, &httpRouteList); err != nil {
 		return reconcile.Result{}, err
@@ -67,10 +67,28 @@ func (gr *GatewayReconciler) Reconcile(ctx context.Context, req reconcile.Reques
 	return reconcile.Result{}, nil
 }
 
+func httpRouteToGateway(ctx context.Context, obj client.Object) []reconcile.Request {
+	hr, ok := obj.(*gwv1.HTTPRoute)
+	if !ok {
+		return nil
+	}
+	var requests []reconcile.Request
+	for _, ref := range hr.Spec.ParentRefs {
+		ns := hr.Namespace
+		if ref.Namespace != nil {
+			ns = string(*ref.Namespace)
+		}
+		requests = append(requests, reconcile.Request{
+			NamespacedName: client.ObjectKey{Namespace: ns, Name: string(ref.Name)},
+		})
+	}
+	return requests
+}
+
 func (gr *GatewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&gwv1.Gateway{}).
-		Watches(&gwv1.HTTPRoute{}, handler.EnqueueRequestForOwner(mgr.GetScheme(), mgr.GetRESTMapper(), &gwv1.Gateway{})).
+		Watches(&gwv1.HTTPRoute{}, handler.EnqueueRequestsFromMapFunc(httpRouteToGateway)).
 		Complete(gr)
 }
 
